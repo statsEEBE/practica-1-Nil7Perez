b3554a135270d0e22bb5d08ed0277a274edc1bfa
nil.perez <nil.perez@auleseebe.upc.edu>
2024-09-19 11:41:42 +0200

    Clase 1

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,8 @@
 #Codigo para problema 2
+ls()#et fa un prompt de totes les variables definides 
+rm(list=ls()) # et borra totes les variables definides
+
+mis_dades<- mtcars
+hist(mis_dades$mpg)
+pie(mis_dades$cyl)
+pie(table(mis_dades$cyl))
